6c792a6d1296b618e358a8e38e4ea3e0b00ed108
Alejandro Alcocer <saavalencia97@gmail.com>
2026-05-26 17:01:57 +0700

    update cosmic pop-os

--- bs.sh
+++ bs.sh
@@ -4,30 +4,30 @@ sudo apt-get install btop -y
 sudo apt-get install git -y
 sudo apt-get install vim -y
 sudo apt-get install neovim -y
-sudo apt-get install dconf-editor -y
 sudo apt-get install icdiff -y
-sudo apt-get install gnome-shell-extension-manager -y
 sudo apt-get install netstat -y
-sudo apt-get install openjdk-17-jdk -y
-sudo apt install gnome-tweaks -y
+# sudo apt-get install dconf-editor -y
+# sudo apt-get install gnome-shell-extension-manager -y
+# sudo apt-get install openjdk-17-jdk -y
+# sudo apt install gnome-tweaks -y
 sudo apt install vim-gtk -y
 sudo apt install ripgrep -y
 
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
-&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
+# (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+# && sudo mkdir -p -m 755 /etc/apt/keyrings \
+# && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+# && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+# && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+# && sudo apt update \
+# && sudo apt install gh -y
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+# https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+#  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 sudo apt-get install zsh -y
 zsh --version
@@ -37,22 +37,22 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-ln -s stalynAlejandro/.dotfiles/.zshrc .
-ln -s stalynAlejandro/.dotfiles/.vimrc .
-ln -s stalynAlejandro/.dotfiles/.gitconfig .
+ln -s salcocer/.dotfiles/.zshrc .
+ln -s salcocer/.dotfiles/.vimrc .
+ln -s salcocer/.dotfiles/.gitconfig .
 
-curl -fsSL https://ollama.com/install.sh | sh
+# curl -fsSL https://ollama.com/install.sh | sh
 
-gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
-gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
+# gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
+# gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
 
-git clone https://github.com/cilynx/rtl88x2bu.git
-cd rtl88x2bu
-VER=$(sed -n 's/\PACKAGE_VERSION="\(.*\)"/\1/p' dkms.conf)
-sudo rsync -rvhP ./ /usr/src/rtl88x2bu-${VER}
-sudo dkms add -m rtl88x2bu -v ${VER}
-sudo dkms build -m rtl88x2bu -v ${VER}
-sudo dkms install -m rtl88x2bu -v ${VER}
-sudo modprobe 88x2bu
+# git clone https://github.com/cilynx/rtl88x2bu.git
+# cd rtl88x2bu
+# VER=$(sed -n 's/\PACKAGE_VERSION="\(.*\)"/\1/p' dkms.conf)
+# sudo rsync -rvhP ./ /usr/src/rtl88x2bu-${VER}
+# sudo dkms add -m rtl88x2bu -v ${VER}
+# sudo dkms build -m rtl88x2bu -v ${VER}
+# sudo dkms install -m rtl88x2bu -v ${VER}
+# sudo modprobe 88x2bu
 
 sudo apt autoremove
